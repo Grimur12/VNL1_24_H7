f15844fb139c3fec1fbcd391de14f662e48cbc99
Grimur12 <dgrimur@gmail.com>
2024-11-29 11:32:50 +0000

    Data layer structure complete

---
+++ src/Data_Layer/DataLayerAPI.py
@@ -0,0 +1,9 @@
+from EmployeesDBLogic import EmployeesDBLogic
+from MaintenanceDBLogic import MaintenanceDBLogic
+from PropertiesDBLogic import PropertiesDBLogic
+
+class DataLayerAPI:
+    def __init__(self):
+        pass
+    
+
