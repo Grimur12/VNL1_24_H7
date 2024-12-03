ad9f2fe3cc00619f6ab38f69ee740bda92a3ebc6
manielvar <manit22@ru.is>
2024-12-03 13:35:49 +0000

    propertyDBcomplete

--- src/Data_Layer/PropertiesDBLogic.py
+++ src/Data_Layer/PropertiesDBLogic.py
@@ -13,20 +13,34 @@ class PropertiesDBLogic:
         """ Function loads all saved properties from DB and turns back into class instances of Property and saves it internally """
         with open(self.file_path, "r") as propertyDBOpen:
             property_list = json.load(propertyDBOpen)
-        # Params 8 is a reference to "type" variable in the classes
         for property in property_list:
             self.properties.append(Property(*property.values()))
 
-    def createProperty() -> None:
-        pass
+    def createProperty(self, params) -> None:
+        """ This function takes in a list of parameters and creates a property and stores in the json DB """
+        self.properties.append(Property(*params))
 
-    def removeProperty() -> None:
-        pass
+    def removeProperty(self, ID) -> None:
+        """ We take in the ID of the property we want to remove, find it, delete it from the internal list and save the internal list to DB """
+        index_to_remove = -1
+        for index, property in enumerate(self.property):
+            if property.employeeID == ID:
+                index_to_remove = index
+                break
+        # Remove that property from the internal list
+        if index_to_remove != -1:
+            del self.properties[index_to_remove]
+        # Save the modified internal list to the DB
+        self.saveProperties()
     
-    def updateProperty() -> None:
-        pass
+    def updateProperty(self, params) -> None:
+        """ This function takes in a list of parameters, some may be new some may still be the older ones and stores them in the json DB """
+        for index, property in enumerate(self.properties):
+            if property.propertyID == params[0]:
+                self.properties[index] = Property(*params)
+        self.saveProperties()
 
-    def saveProperty(self) -> None:
+    def saveProperties(self) -> None:
         """ Function saves all instances of the Property class saved in self.properties in dictionary form into json Database """
         Property = [property.Property_dict for property in self.properties]
 
