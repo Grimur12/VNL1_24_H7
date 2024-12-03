e50550095a255569a40f0fdfe95739819969df66
idunnmariag <idunn@mumu.is>
2024-12-03 15:26:49 +0000

    .

--- src/Logic_Layer/LogicLayerEmployeeLogic.py
+++ src/Logic_Layer/LogicLayerEmployeeLogic.py
@@ -1,29 +1,6 @@
-#
 
-from Data_Layer import DataLayerAPI
 
-class LogicLayerEmployeeLogic:
-    def __init__(self, ):
-        pass
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-    def addEmployee(self, ):
-=======
-    def addEmployee(self,params):
->>>>>>> 6fd890fe27a66e5865eea8db13eca8af3f476e4f
-        pass
 
-    def removeEmployee(self, ):
-        pass
 
-    def filterEmployee(self, ):
-        pass
 
-    def getEmployeeData(self, ):
-        pass
-
-    def checkEmployeeErrors(self, ):
-        pass
-=======
->>>>>>> Stashed changes
