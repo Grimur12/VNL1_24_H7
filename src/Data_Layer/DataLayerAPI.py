780684237fbf587b218755135eb6b3455b3789cb
Grimur12 <152170365+Grimur12@users.noreply.github.com>
2024-12-03 18:48:14 +0000

    Update DataLayerAPI.py

--- src/Data_Layer/DataLayerAPI.py
+++ src/Data_Layer/DataLayerAPI.py
@@ -18,7 +18,7 @@ class DataLayerAPI:
     def removeEmployee(self, ID) -> None:
         EmployeesDBLogic.removeEmployee(ID)
 
-    def propagateData(self) -> None:
+    def propagateEmployeeData(self) -> None:
         return EmployeesDBLogic.propagateData()
     
     def loadPropertiesLog(self) -> None:
@@ -33,7 +33,37 @@ class DataLayerAPI:
     def updateProperty(self, params) -> None:
         PropertiesDBLogic.updateProperty(params)
     
-    def propagateData(self) -> list:
+    def propagatePropertyData(self) -> list:
         return PropertiesDBLogic.propagateData()
+
+    def loadMaintenanceLog(self) -> None:
+        MaintenanceDBLogic.loadMaintenanceLog()
+
+    def loadMaintenanceSchedule(self) -> None:
+        MaintenanceDBLogic.loadMaintenanceSchedule()
+        
+    def updateMaintenanceStatus(self, params) -> None:
+        MaintenanceDBLogic.updateMaintenanceStatus(params)
+
+    def updateMaintenanceSchedule(self, params) -> None:
+        MaintenanceDBLogic.updateMaintenanceSchedule(params)
+
+    def createMaintenanceSchedule(self, params) -> None:
+        MaintenanceDBLogic.createMaintenanceSchedule(params)
+
+    def createMaintenance(self, params) -> None:
+        MaintenanceDBLogic.createMaintenance(params)
+
+    def removeMaintenance(self, ID) -> None:
+        MaintenanceDBLogic.removeMaintenance(ID)
+
+    def removeMaintenanceSchedule(self, ID) -> None:
+        MaintenanceDBLogic.removeMaintenanceSchedule(ID)
+
+    def propagateMaintenanceData(self) -> list:
+        return MaintenanceDBLogic.propagateMaintenanceData()
+
+    def propagateMaintenanceScheduleData(self) -> list:
+        return MaintenanceDBLogic.propagateMaintenanceScheduleData()
 
 
