dd1780f487079510e503d2dbca3cd570933346d7
Grimur12 <dgrimur@gmail.com>
2024-12-03 14:29:24 +0000

    Property tests, Wrapper added

--- src/Data_Layer/DataLayerAPI.py
+++ src/Data_Layer/DataLayerAPI.py
@@ -6,7 +6,34 @@ class DataLayerAPI:
     def __init__(self):
         pass
 
-    def addEmployee(self, params):
+    def addEmployee(self, params) -> None:
         EmployeesDBLogic.createEmployee(params)
+
+    def loadEmployeeLog(self) -> None:
+        EmployeesDBLogic.loadEmployeeLog()
+
+    def updateEmployee(self, params) -> None:
+        EmployeesDBLogic.updateEmployee(params)
+
+    def removeEmployee(self, ID) -> None:
+        EmployeesDBLogic.removeEmployee(ID)
+
+    def propagateData(self) -> None:
+        return EmployeesDBLogic.propagateData()
     
+    def loadPropertiesLog(self) -> None:
+        PropertiesDBLogic.loadPropertiesLog()
+
+    def createProperty(self, params) -> None:
+        PropertiesDBLogic.createProperty(params)
+
+    def removeProperty(self, ID) -> None:
+        PropertiesDBLogic.removeProperty(ID)
+
+    def updateProperty(self, params) -> None:
+        PropertiesDBLogic.updateProperty(params)
+    
+    def propagateData(self) -> list:
+        return PropertiesDBLogic.propagateData()
+
 
