20f753d149eb3c91ce791e708b5279a1d4978838
Grimur12 <152170365+Grimur12@users.noreply.github.com>
2024-12-03 18:32:06 +0000

    Update MaintenanceSchedule.py

--- src/Models/MaintenanceSchedule.py
+++ src/Models/MaintenanceSchedule.py
@@ -1,9 +1,9 @@
 #defining the Maintenance schedule classes.
 
 class MaintenanceSchedule:
-    def __init__(self, maintenanceSchedule_ID, maintenanceIDreference, employee_ID, taskType, frequency):
+    def __init__(self, ID, maintenanceIDreference, employee_ID, taskType, frequency):
         """ Defines variables for Maintenance Schedule """
-        self.maintenanceSchedule_ID = maintenanceSchedule_ID
+        self.maintenanceScheduleID = maintenanceScheduleID
         self.maintenanceIDreference = maintenanceIDreference
         self.employee_ID = employee_ID
         self.taskType = taskType
@@ -12,7 +12,7 @@ class MaintenanceSchedule:
     def maintenanceSchedule_Dict(self) -> dict:
         """ Returns all the variables in our Maintenance Schedule class into a dictionary """
         return {
-        "maintenanceSchedule _ID": self.maintenanceSchedule_ID,
+        "maintenanceScheduleID": self.maintenanceScheduleID,
         "maintenanceIDreference": self.maintenanceIDreference,
         "employee_ID": self.employee_ID,
         "taskType": self.taskType,
