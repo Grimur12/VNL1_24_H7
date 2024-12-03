29b0fcfd03a68be2ebbf6013055b54cff12cc510
manielvar <manit22@ru.is>
2024-12-03 14:58:02 +0000

    MaintenanceFunctions

--- src/Data_Layer/MaintenanceDBLogic.py
+++ src/Data_Layer/MaintenanceDBLogic.py
@@ -1,30 +1,45 @@
 # Maintenance DB Logic
+import os
+import json
+from Models.Maintenance import Maintenance
+from Models.MaintenanceSchedule import MaintenanceSchedule
 
 class MaintenanceDBLogic:
 
     def __init__(self):
+        self.maintenance = []
+        self.maintenanceSchedule = []
+        self.base_dir = os.path.dirname(os.path.dirname(__file__))
+        self.maintenance_file_path = os.path.join(self.base_dir, "Data_Layer/Databases", "Maintenance.json")
+        self.maintenance_Schedule_file_path = os.path.join(self.base_dir, "Data_Layer/Databases", "MaintenanceSchedule.json")
+
+    def loadMaintenanceLog(self) -> None:
+        """ Function loads all saved maintenances from DB and turns back into class instances of Maintenance and saves it internally """
+        with open(self.maintenance_file_path, "r") as maintenanceDBOpen:
+            maintenance_list = json.load(maintenanceDBOpen)
+        for maint in maintenance_list:
+            self.maintenance.append(Maintenance(*maint.values()))
+
+    def loadMaintenanceSchedule(self) -> None:
+        with open(self.maintenance_Schedule_file_path, "r") as maintenanceScheduleDBOpen:
+            maintenanceSchedule_list = json.load(maintenanceScheduleDBOpen)
+        for schedule in maintenanceSchedule_list:
+            self.maintenance.append(MaintenanceSchedule(*schedule.values()))
+
+    def updateMaintenanceStatus(self) -> None:
         pass
 
-    def loadMaintenanceLog() -> None:
+    def updateMaintenanceSchedule(self) -> None:
         pass
 
-    def loadMaintenanceSchedule() -> None:
+    def createMaintenanceSchedule(self) -> None:
         pass
 
-    def updateMaintenanceStatus() -> None:
+    def createMaintenance(self) -> None:
         pass
 
-    def updateMaintenanceSchedule() -> None:
+    def removeMaintenance(self) -> None:
         pass
 
-    def createMaintenanceSchedule() -> None:
-        pass
-
-    def createMaintenance() -> None:
-        pass
-
-    def removeMaintenance() -> None:
-        pass
-
-    def saveMaintenance() -> None:
+    def saveMaintenance(self) -> None:
         pass
--- src/Models/MaintenanceSchedule.py
+++ src/Models/MaintenanceSchedule.py
@@ -1,9 +1,10 @@
 #defining the Maintenance schedule classes.
 
 class MaintenanceSchedule:
-    def __init__(self, maintenanceSchedule_ID, employee_ID, taskType, frequency):
+    def __init__(self, maintenanceSchedule_ID, maintenanceIDreference, employee_ID, taskType, frequency):
         """ Defines variables for Maintenance Schedule """
         self.maintenanceSchedule_ID = maintenanceSchedule_ID
+        self.maintenanceIDreference = maintenanceIDreference
         self.employee_ID = employee_ID
         self.taskType = taskType
         self.frequency = frequency
@@ -12,6 +13,7 @@ class MaintenanceSchedule:
         """ Returns all the variables in our Maintenance Schedule class into a dictionary """
         return {
         "maintenanceSchedule _ID": self.maintenanceSchedule_ID,
+        "maintenanceIDreference": self.maintenanceIDreference,
         "employee_ID": self.employee_ID,
         "taskType": self.taskType,
         "frequency": self.frequency
