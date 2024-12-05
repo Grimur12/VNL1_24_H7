3a0e02ab79bd301be68c7d3081dc629aa91f53e3
idunnmariag <idunn@mumu.is>
2024-12-05 14:39:27 +0000

    .

--- src/UI_Layer/ContractorUILogic.py
+++ src/UI_Layer/ContractorUILogic.py
@@ -2,19 +2,18 @@
 #from Logic_Layer import LogicLayerAPI
 
 class ContractorUILogic:
-    def __init__(self):
-        pass
+        def __init__(self):
+                RED = "\33[91m"
+                BLUE = "\33[94m"
+                GREEN = "\033[32m"
+                YELLOW = "\033[93m"
+                PURPLE = '\033[0;35m' 
+                CYAN = "\033[36m"
+                END = "\033[0m"
 
-RED = "\33[91m"
-BLUE = "\33[94m"
-GREEN = "\033[32m"
-YELLOW = "\033[93m"
-PURPLE = '\033[0;35m' 
-CYAN = "\033[36m"
-END = "\033[0m"
-
-banner = f"""
-  {BLUE}
+        def display_banner(self):
+                banner = f"""
+                {self.BLUE}
           _____                    _____                    _____                            _____                    _____                    _____          
          /\    \                  /\    \                  /\    \                          /\    \                  /\    \                  /\    \         
         /::\____\                /::\    \                /::\____\                        /::\    \                /::\    \                /::\    \        
