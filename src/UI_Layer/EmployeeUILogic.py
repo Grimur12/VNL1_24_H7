48c6189383041fcedea179b2dcd2cc1b419a3393
idunnmariag <idunn@mumu.is>
2024-12-05 14:41:06 +0000

    .

--- src/UI_Layer/EmployeeUILogic.py
+++ src/UI_Layer/EmployeeUILogic.py
@@ -3,19 +3,20 @@
 
 class EmployeeUILogic:
     def __init__(self):
-        RED = "\33[91m"
-        BLUE = "\33[94m"
-        GREEN = "\033[32m"
-        YELLOW = "\033[93m"
-        PURPLE = '\033[0;35m' 
-        CYAN = "\033[36m"
-        END = "\033[0m"
-        
 
-    def display_banner(self):
-        print(f"  {BLUE}  
-              
-                      _____                    _____                    _____                            _____                    _____                    _____          
+class ContractorUILogic:
+    def __init__(self):
+
+RED = "\33[91m"
+BLUE = "\33[94m"
+GREEN = "\033[32m"
+YELLOW = "\033[93m"
+PURPLE = '\033[0;35m' 
+CYAN = "\033[36m"
+END = "\033[0m"
+
+banner = f"""
+  {BLUE}          _____                    _____                    _____                            _____                    _____                    _____          
          /\    \                  /\    \                  /\    \                          /\    \                  /\    \                  /\    \         
         /::\____\                /::\    \                /::\____\                        /::\    \                /::\    \                /::\    \        
        /::::|   |               /::::\    \              /::::|   |                       /::::\    \               \:::\    \              /::::\    \       
@@ -37,7 +38,8 @@ class EmployeeUILogic:
         \::/    /                \::/    /                \::/    /                        \::/    /                \::/    /                \:|   |          
          \/____/                  \/____/                  \/____/                          \/____/                  \/____/                  \|___|          
                                                                                                                                                               """
-")
+
+print(banner)
 
     def display_menu(self):
         pass
