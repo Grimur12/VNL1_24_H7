831d1797f1fddd4c09e0b956a25d0338721574d0
pedro3apl <pedro24@ru.is>
2024-12-10 15:37:43 +0000

    .

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ from UI_Layer.ContractorUILogic import ContractorUILogic
 from UI_Layer.EmployeeUILogic import EmployeeUILogic
 from UI_Layer.ManagerUILogic import ManagerUILogic
 from UI_Layer.ViewUILogic import ViewUILogic
-#from prettytable import PrettyTable
-
 
 class main:
     def __init__(self):
@@ -39,37 +37,4 @@ if __name__ == "__main__":
             start.ContractorUI.run()
         else:
             invalid = True
-
-# if __name__ == "__main__":
-#     start = main()
-#     invalid = False
-
-#     while True: 
-#         start.ViewUI.clearTerminal()
-
-
-#         menu_table = PrettyTable()
-#         menu_table.field_names = ["What are you?"]
-#         menu_table.add_row(["1", "If you are a Manager"])
-#         menu_table.add_row(["2", "If you are an Employee"])
-#         menu_table.add_row(["3", "If you are a Contractor"])
-#         menu_table.add_row(["Q", "To Quit"])
-
-#         print(menu_table)
-
-#         if invalid:
-#             print("Error: Invalid Input\n")
-
-#         initial_input = input("Choice: ")
-#         if initial_input.lower() == "q":
-#             print("Quitting")
-#             break
-#         elif initial_input == "1":
-#             start.ManagerUI.run()
-#         elif initial_input == "2":
-#             start.EmployeeUI.run()
-#         elif initial_input == "3":
-#             start.ContractorUI.run()
-#         else:
-#             invalid = True
             
